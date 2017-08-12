c0f54328ab89ec4a7eae205b65b3d2d13dcfced3
bernardw01 <bernard.l.williams@live.com>
2017-08-12 09:11:13 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,4 +1,51 @@
-var express = require('expresss');
-var method = require('method-override');
-var body = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var http = require('http');
 
+var app = express();
+var port = normalizePort(process.env.PORT || '3020');
+app.set('port', port);
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
+
+app.use("/", routes);
+
+app.listen(port);
+
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
